f37e71a662b298b488c823c09943e574642ebd39
lastnamehurt <clhurt1@gmail.com>
2022-07-08 21:09:39 -0700

    cleanup

--- quotes/models.py
+++ quotes/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Quote(models.Model):
-    company_name = models.CharField(max_length=200, null=True, blank=True)
+from phonenumber_field.modelfields import PhoneNumberField
 
+
+class Quote(models.Model):
+    id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=200, null=True, blank=True)
+    email_address = models.EmailField(null=True, blank=True)
+    phone_number = PhoneNumberField(blank=True, null=True)
+    estimated_distance = models.CharField(max_length=200, blank=True, null=True)
+    container_type = models.CharField(max_length=200, blank=True, null=True)
+    is_legal_weight = models.BooleanField(default=True)
+    is_hazmat = models.BooleanField(default=False)
+    additional_details = models.TextField(default="")
