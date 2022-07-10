6efba94b99a3df4efa8a2fae1676a3cb852ca2dd
lastnamehurt <clhurt1@gmail.com>
2022-07-09 17:35:06 -0700

    debug false

--- quotes_api/settings.py
+++ quotes_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^s3)zda74ye65!g$08qkkfxn-nztk39(6^30-aspabt-0r1qbw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
